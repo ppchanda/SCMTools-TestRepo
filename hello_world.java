67be51fa1c5619425f4107c82a55b78008ecc424
pankaj <pankaj.chanda@ril.com>
2018-04-13 06:19:40 -0400

    Initial checking of 3 source programs in 3 languages

---
+++ hello_world.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        boolean learning = true;
+ 
+    if (learning) {
+      System.out.println("Java programmer");
+    }
+    else {
+      System.out.println("What are you doing here?");
+    }
+    }
+}
+
